014a414391c8fabf58c61c6d8543ef9e0f369778
Padilla <francisco.j.padilla@accenture.com>
2018-01-29 11:05:50 -0600

    agrgamos todos los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+import { HomeModule } from './views/home/home.module';
+import { LoginModule } from './views/login/login.module';
+import { ProductosModule } from './views/productos/productos.module';
+
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    LoginModule,
+    NgbModule.forRoot(),
+    ProductosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
